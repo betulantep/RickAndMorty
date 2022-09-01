ddc70a1d83f366d6a981abbb99f168d594493010
Betul Antep <betulantepp@gmail.com>
2022-09-01 14:14:49 +0300

    episode entities, nav graph, fix bottom nav

---
+++ app/src/main/java/com/betulantep/rickandmorty/data/entities/episode/Info.kt
@@ -0,0 +1,15 @@
+package com.betulantep.rickandmorty.data.entities.episode
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Info(
+    @SerializedName("count")
+    val count: Int,
+    @SerializedName("next")
+    val next: String,
+    @SerializedName("pages")
+    val pages: Int,
+    @SerializedName("prev")
+    val prev: Any
+)
